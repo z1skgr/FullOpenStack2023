c34a70243353744c373f2a60dd0d9c14991493a8
C.Ziskas <z1skgr@yahoo.gr>
2023-02-23 23:19:34 +0200

    Ex 1.8
    the statistics is extracted into its own Statistics component. The state of the application should remain in the App root component.

--- Part 1/unicafe/src/App.js
+++ Part 1/unicafe/src/App.js
@@ -2,85 +2,77 @@ import React from "react"
 
 import {useState} from 'react'
 
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
+
+const Header = props => <h1>{props.name}</h1>
+
+
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
     {text}
   </button>
 )
-const Label = ({text, value, special}) => {
-  return (
-    <div>
-      {text} {value} {special}
-    </div>
-  )
-}
 
-const Header = (props) => <h1>{props.name}</h1>
 
+
+const Statistics = ({stats}) => {
+  const total = stats.good + stats.neutral + stats.bad
+
+  let average = (stats.good * 1 + stats.bad * -1) / total
+  let positive = stats.good * (100/total)
+
+  if(total===0){
+    average=0
+    positive=0
+  }
+
+
+  return ( 
+  <div> 
+    <div>good {stats.good}</div> 
+    <div>neutral {stats.neutral}</div> 
+    <div>bad {stats.bad}</div> 
+    <div>all {total}</div> 
+    <div>average {average}</div>           
+    <div>positive {positive} %</div>
+  </div>
     
+    
+  )
 
+}
 
+const App = () =>{
+  const [stats, setStats] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
 
+  const handleGood = () =>
+    setStats({...stats, good: stats.good + 1})
 
-const App = () => {
+  const handleNeutral = () =>
+    setStats({...stats, neutral: stats.neutral + 1})
 
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [avg, setAvg] = useState(0)
-  const [pos, setPos] = useState(0)
-
-  
-  const handleGood = () =>{
-    setGood(good + 1)
-    setTotal(total+1)
-    setAvg(((good+1)*1+ (bad*(-1)))/(total+1))
-    setPos((good+1)/(total+1)*100)
-  }
-  const handleNeutral = () =>{
-    setNeutral(neutral + 1)
-    setTotal(total+1)
-    setAvg((good*1+ (bad*(-1)))/(total+1))
-    setPos(good/(total+1)*100)
-  }
-  const handleBad = () =>{
-    setBad(bad + 1)
-    setTotal(total+1)
-    setAvg((good*1+ ((bad+1)*(-1)))/(total+1))
-    setPos(good/(total+1)*100)
-
-  }
-
-
-
-
-
-
- 
-
+  const handleBad = () =>
+    setStats({...stats, bad: stats.bad + 1}) 
 
   return (
     <div>
+      <Header name="Give feedback" />
+      <Button onClick={handleGood} text='good' />
+      <Button onClick={handleNeutral} text='neutral' />
+      <Button onClick={handleBad} text='bad' />
+      <Header name="Statistics" />
 
-     <Header name="Give feedback" Header/>
-      <Button handleClick={handleGood} text="good" />
-      <Button handleClick={handleNeutral} text="neutral" />
-      <Button handleClick={handleBad} text="bad" />
-      <h1>Statistics</h1>
-      <div>
-        <Label text="good" value={good} special={""}/>
-        <Label text="neutral" value={neutral} special={""}/>
-        <Label text="bad" value={bad} special={""}/>
-        <Label text="all" value={total} special={""} />
-        <Label text="average" value={avg} special={""} />
-        <Label text="positive" value={pos} special={"%"} />
-
-      </div>
+      <Statistics stats={stats} />
     </div>
+    
+    
   )
+    
 }
+
+
 
 export default App
 
