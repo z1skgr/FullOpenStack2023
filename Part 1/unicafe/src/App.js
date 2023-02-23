9fd1d8ee8b4ed3c629e1e665005510e6b5ef7f89
C.Ziskas <z1skgr@yahoo.gr>
2023-02-23 22:50:30 +0200

    Ex 1.7
    Expand your application so that it shows more statistics about the gathered feedback: the total number of collected feedback, the average score (good: 1, neutral: 0, bad: -1) and the percentage of positive feedback.

--- Part 1/unicafe/src/App.js
+++ Part 1/unicafe/src/App.js
@@ -7,13 +7,15 @@ const Button = ({handleClick, text}) => (
     {text}
   </button>
 )
-const Label = ({text, value}) => {
+const Label = ({text, value, special}) => {
   return (
     <div>
-      {text} {value}
+      {text} {value} {special}
     </div>
   )
 }
+
+const Header = (props) => <h1>{props.name}</h1>
 
     
 
@@ -26,32 +28,55 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [pos, setPos] = useState(0)
 
   
   const handleGood = () =>{
     setGood(good + 1)
+    setTotal(total+1)
+    setAvg(((good+1)*1+ (bad*(-1)))/(total+1))
+    setPos((good+1)/(total+1)*100)
   }
   const handleNeutral = () =>{
     setNeutral(neutral + 1)
+    setTotal(total+1)
+    setAvg((good*1+ (bad*(-1)))/(total+1))
+    setPos(good/(total+1)*100)
   }
   const handleBad = () =>{
     setBad(bad + 1)
+    setTotal(total+1)
+    setAvg((good*1+ ((bad+1)*(-1)))/(total+1))
+    setPos(good/(total+1)*100)
+
   }
+
+
+
+
+
 
  
 
 
   return (
     <div>
-      <h1>Give feedback</h1>
+
+     <Header name="Give feedback" Header/>
       <Button handleClick={handleGood} text="good" />
       <Button handleClick={handleNeutral} text="neutral" />
       <Button handleClick={handleBad} text="bad" />
       <h1>Statistics</h1>
       <div>
-        <Label text="good" value={good}/>
-        <Label text="neutral" value={neutral}/>
-        <Label text="bad" value={bad}/>
+        <Label text="good" value={good} special={""}/>
+        <Label text="neutral" value={neutral} special={""}/>
+        <Label text="bad" value={bad} special={""}/>
+        <Label text="all" value={total} special={""} />
+        <Label text="average" value={avg} special={""} />
+        <Label text="positive" value={pos} special={"%"} />
+
       </div>
     </div>
   )
