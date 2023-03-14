64bd28f9e4ef49c927329d0bdb8ede698f5cf8a1
C.Ziskas <z1skgr@yahoo.gr>
2023-03-14 23:19:17 +0200

    Ex2.5
    Declare the Course component as a separate module, which is imported by the App component. You can include all subcomponents of the course in the same module.

--- Part 2/courseinfo/src/App.js
+++ Part 2/courseinfo/src/App.js
@@ -1,42 +1,5 @@
 import React from "react"
-
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.text}</h1>        
-}
-
-const Label = (props) => {
-  console.log(props)
-  return (
-    <div>
-     <p> {props.text} {props.value}</p>
-    </div>
-  )
-
-}
-
-
-const Course = ({ course }) => {
-
-  const total = course.parts.reduce((prev, current) => {
-    return prev + current.exercises;
-  }, 0);
-    return (
-      <div><Header text={course.name} />
-      {course.parts.map(cc => 
-        <Label text={cc.name} value={cc.exercises}/>
-                     
-      )}<p> <b>Total of {total}  exercises</b></p>
-        
-      
-
-      </div>
-    )
-
-    
-  }
-
-
+import Course from "./components/Course"
 
 
   
