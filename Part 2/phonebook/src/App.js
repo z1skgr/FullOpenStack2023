2cf6bf7f9aba5f3b6574096c8478b2e77be0441e
C.Ziskas <z1skgr@yahoo.gr>
2023-03-15 00:01:59 +0200

    Ex2.8
    Expand your application by allowing users to add phone numbers to the phone book. You will need to add a second input element to the form (along with its own event handler):

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -2,51 +2,74 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      id: 1
-   }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+
+  const addPerson = (event) => {
     event.preventDefault()
     const found = persons.findIndex((element) => element.name === newName)
     //console.log(`${found} index`)
     
     if(found>-1){
-      alert(`${newName} is already in phonebook`)
       alert(newName + ' is already added to phonebook')
     }else{
       const nameObject = {
         name: newName,
+        number: newNumber,
         id: persons.length+1
       }
     
       setPersons(persons.concat(nameObject))
+
     }
     setNewName('')
+    setNewNumber('')
     
   }
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
+
+  const handleNameChange = (e) => {
+    console.log(e.target.value)
+    setNewName(e.target.value)
   }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+
+
+
+    
+    
+  
   //debugger
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+          </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       
       <h2>Numbers</h2>
-        {persons.map(per => <li key={per.id}>{per.name}</li>)}
+        {persons.map(per => <li key={per.id}>{per.name} {per.number}</li>)}
+        <div> 
+        </div>
+     
     </div>
   )
 }
