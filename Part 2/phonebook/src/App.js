cbf4677adb7a38669abec89b90f84d07a06a6420
C.Ziskas <z1skgr@yahoo.gr>
2023-03-15 16:59:27 +0200

    Ex2.11
    We continue with developing the phonebook. Store the initial state of the application in the file db.json, which should be placed in the root of the project.
    
    Start json-server on port 3001 and make sure that the server returns the list of people by going to the address http://localhost:3001/persons in the browser.

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -1,19 +1,27 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Person from './components/Person'
 import Filter from './components/Filter'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [initial, setInitial] = useState(persons)
+
+    // Fetch person data from json-server
+    useEffect(() => {
+      console.log('effect')
+      axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          console.log('promise fulfilled')
+          setPersons(response.data)
+          setInitial(response.data)
+        })
+    }, [])
   
 
   const addPerson = (event) => {
