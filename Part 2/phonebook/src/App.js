af0d8797501515967ab5c025e75785038ba87e1b
C.Ziskas <z1skgr@yahoo.gr>
2023-03-16 22:27:22 +0200

    Ex2.17
    If you delete a person in browser 1 a short while before attempting to change the person's phone number in browser 2, you will get the following error message:
    
    Fix the issue according to the example shown in promise and errors in part 2. Modify the example so that the user is shown a message when the operation does not succeed. The messages shown for successful and unsuccessful events should look different:

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -5,8 +5,6 @@ import Filter from './components/Filter'
 import personService from "./services/persons"
 import Message from "./components/Message";
 import "./index.css"
-//To this
-
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
@@ -71,9 +69,15 @@ const App = () => {
         setPersons(updatedPersons);
         setInitial(updatedPersons);
         setMessage(`Updated ${newName}'s number`);}
-        ).catch(error=>{setMessage(
-          `[ERROR] ${error.response.data.error}`
-        )});
+        ).catch(error=>{
+          console.log(error)
+              setPersons(persons.filter(person => person.id !== findObject.id))
+              setNewName('')
+              setNewNumber('')
+              setMessage(
+                `[ERROR] ${findObject.name} was already deleted from server`
+              )
+        })
         //console.log(`Updated ${newName}'s number to ${newNumber}`  );
           
       }else{
@@ -82,11 +86,11 @@ const App = () => {
       }
     }else{
 
-      const num = persons.find((person) => person.id === persons.length+1)
+      
       const nameObject = {
         name: newName,
         number: newNumber,
-        id: num ? Math.max(persons.map(pp=>pp.id))+1 : persons.length+1
+        id: persons.length+1
       }
 
       
@@ -107,9 +111,7 @@ const App = () => {
           setInitial(persons.concat(returnedPerson))
           setMessage(`Added ${newName} to phone-book`)
           console.log(`Added ${returnedPerson.name} to phone-book with number ${returnedPerson.number}`)
-        }).catch(error=>{setMessage(
-          `[ERROR] ${error.response.data.error}`
-        )})
+        })
       }
       else{
         setMessage(`[ERROR] Invalid  number`);
@@ -180,9 +182,7 @@ const App = () => {
         setInitial(updatedPersons);
         setMessage(`Deleted ${name} from phone_book`);
         console.log(`Deleted ${name} from phone_book`);
-      }).catch(error=>{setMessage(
-        `[ERROR] ${error.response.data.error}`
-      )})
+      })
         console.log('delete')
     }
 
@@ -194,7 +194,7 @@ const App = () => {
   return (
     
       <div>
-      <h1>Phone Book</h1>
+      <h2>Phone Book</h2>
       
       <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
       
