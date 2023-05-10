794509ebd8fcf7fdc0218bf689ad4e36f2abc4a0
C.Ziskas <z1skgr@yahoo.gr>
2023-05-10 17:20:19 +0300

    Ex 3.12
    Create a mongo.js file in the project directory, that can be used for adding entries to the phonebook, and for listing all of the existing entries in the phonebook.

---
+++ Part 3/phonebook-backend/mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://cziskas:${password}@fullstackcluster.ur8whus.mongodb.net/mongo?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    id_number: String,
+    name: String, 
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    id_number:  Math.floor(1 + (Math.random() * (99))),
+    name: name, 
+    number: number
+})
+
+
+
+
+if(process.argv.length===3){
+  Person.find({}).then(result => {
+    console.log('Phonebook')
+    result.forEach(data => {
+      console.log(`${data.name} ${data.number}`)
+    })
+    mongoose.connection.close()
+  })
+}else if(process.argv.length===5){
+  person.save().then(result => {
+    console.log(`Added ${name} with number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}else{
+  console.log(`Invalid Parameters`)
+  mongoose.connection.close()
+}
