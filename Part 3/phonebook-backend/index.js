41e2d4e2853b93329c4603b7ec0d55de587d8935
C.Ziskas <z1skgr@yahoo.gr>
2023-05-11 18:36:09 +0300

    Ex 3.13-14
    Ex3-13
    3.13- Change the fetching of all phonebook entries so that the data is fetched from the database. Verify that the frontend works after the changes have been made.
    
    3.14 - Change the backend so that new numbers are saved to the database. Verify that your frontend still works after the changes.

--- Part 3/phonebook-backend/index.js
+++ Part 3/phonebook-backend/index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+
 const app = express()
-
-
 const morgan = require('morgan')
+
 app.use(express.json())
+app.use(express.static('build'))
+
+const cors = require('cors')
+
+require('dotenv').config()
+const Person = require('./models/person')
+
+app.use(cors())
+
+morgan('tiny')
 
 morgan.token("data", (request) => {
   return request.method === "POST" ? JSON.stringify(request.body) : " ";
@@ -13,124 +23,102 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 
-morgan('tiny')
-
-
+app.use(cors())
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-
 ]
  
   app.get('/', (request, response) => {
-    response.send('<h1>Hello Exercise 3.7 & 3.8!</h1>')
+    response.send('<h1>Hello Exercise 3.13-14!</h1>')
   })
   
 
   
   app.get('/api/persons', (request, response) => {
-    
-    response.json(persons)
-  })
-
-  app.get('/api/info', (request, response) => {
-    const date_time = new Date().toString(); 
-    response.send(`<p>Phonebook has info for ${persons.length} people.</p><p>${date_time}</p>`);
-  })
-
-  
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const per = persons.find(person => {
-     // console.log(person.id, typeof person.id, id, typeof id, person.id === id)
-      return person.id === id
+    Person.find({}).then(persons => {
+      response.json(persons)
     })
-    if (per) {
-      response.json(per)
-    } else {
-      response.status(400).send( 
-        `error: content missing` 
-      )
-    }
-    //console.log(per)
+  })
 
+    
+  app.get('/api/info', (request, response,next) => {
+    const date_time = new Date().toString(); 
+    Person.find({}).then(persons=>{
+      response.send(`<p>Phonebook has info for ${persons.length} people.</p><p>${date_time}</p>`);
+    }).catch((error)=>next(error));
   })
 
 
+
+
+  app.get('/api/persons/:id', (request, response,next) => {
+    Person.findById(request.params.id).then((people)=>{
+      if(people){
+        response.json(people)
+      }else{
+        response.status(404).end();
+      }
+    }).catch((error)=>next(error));
+
+  })
+/*
   app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(per => per.id !== id)
   
     response.status(204).end()
 
-  })
-
+  })*/
 
   const generateId = () => {
     const Id = Math.floor(Math.random() * 10000000);
     return Id
   }
 
-  app.post('/api/persons', (request, response) => {
+  app.post('/api/persons', (request, response,next) => {
     const body = request.body
+    const name_exists = persons.filter(per => per.name !== body.name)?false:true;
 
-    if (!body.number || !body.name) {
+
+   if (!body.number) {
       return response.status(400).json({ 
-        error: 'content missing' 
+        error: 'number missing' 
       })
-    }else if(persons.filter(per => per.name === body.name)){
+    }else if(!body.name){
       return response.status(400).json({ 
-        error: 'content already in phonebook' 
+        error: 'name missing' 
+      })
+    }
+    else if(name_exists){
+      return response.status(400).json({ 
+        error: 'name must be unique' 
       })
     }
    
-    const new_person = {
-      id: generateId(),
+    const new_person = new Person ({
+      id_number: generateId(),
       name: body.name,
       number: body.number
-    }
+    })
+
+    new_person.save().then((savedPerson)=>{
+      response.json(savedPerson);
+    }).catch((error)=>next(error))
   
-    per = persons.concat(new_person)
-  
-    response.json(per)
+
   })
 
-  app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-    ].join(' ')
-  }))
 
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
+
+
 
   
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
