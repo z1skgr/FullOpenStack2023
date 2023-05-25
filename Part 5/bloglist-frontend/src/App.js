1524dabcf528644e8c7c5bdc9299b2036436263d
C.Ziskas <z1skgr@yahoo.gr>
2023-05-25 17:46:19 +0300

    Exer 5.3
    5.3: Expand your application to allow a logged-in user to add new blogs:

--- Part 5/bloglist-frontend/src/App.js
+++ Part 5/bloglist-frontend/src/App.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
-
+import React from 'react'
 
 import blogService from './services/blogs'
 import loginService from './services/login'
 import loginForm from "./components/LoginForm";
 import blogForm from "./components/BlogForm";
 import "./index.css"
+import Message from "./components/Message";
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -13,11 +14,44 @@ const App = () => {
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
   const [message, setMessage] = useState(null)
+  const [form, setForm] = useState({
+    author: '',
+    title: '',
+    url: '',
+  });
+
+  
+  const handleAuthor = (event) =>{
+    setForm({
+      ...form,
+      author: event.target.value
+    });
+
+  }
+
+  const handleTitle = (event) =>{
+    setForm({
+      ...form,
+      title: event.target.value
+    });
+
+  }
+  
+  const handleUrl = (event) =>{
+    setForm({
+      ...form,
+      url: event.target.value
+    });
+
+  }
+
 
   const handleLogout = async (event) => {
     event.preventDefault()
     window.localStorage.clear()
     setUser(null)
+    setMessage(null)
+    setForm({author:'',title:'',url:''})
   }
 
   const handleLogin = async (event) => {
@@ -34,6 +68,10 @@ const App = () => {
       setUser(user)
       setUsername('')
       setPassword('')
+      setMessage('Login approved')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     } catch (exception) {
       setMessage('ERROR, Wrong credentials')
       setTimeout(() => {
@@ -45,20 +83,59 @@ const App = () => {
 
 
   useEffect(() => {
-    blogService.getAll().then(blogs => {
+    blogService.getAll().then(blogs  => {
       setBlogs( blogs ) }
     )  
   }, [])
+
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+    const newBlog = {
+      title: form.title,
+      author: form.author,
+      url: form.url
+    }
+    console.log(newBlog)
+
+
+    try {
+      await blogService.create(newBlog);
+      setBlogs(blogs.concat(newBlog));
+      
+      setForm({author:'',title:'',url:''})
+      setMessage(`Blog ${newBlog.title} was successfully added`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    } catch(exception) {
+      setMessage(
+        `ERROR, Cannot add blog ${newBlog.title}`
+      )
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }    
+  }
+
   
   return (
     <div>
       <h2>log in to application</h2>
+        {user === null ? null :
+        <p>
+          <span className="active-user">{user.name}</span> logged in
+            
+            <button onClick={handleLogout} type="submit">logout</button>
+        </p>}
 
       {user === null ?
       
-      loginForm(handleLogin, username, password, setUsername, setPassword, message)
-      : blogForm(handleLogout,user,blogs)
-    }      
+        loginForm(handleLogin, username, password, setUsername, setPassword, message)
+      : blogForm(addBlog,blogs,form.author,handleAuthor, form.title, handleTitle,form.url,handleUrl,message)
+
+    }
+   <Message message={message} /> 
     </div>
   )
 }
--- Part 5/bloglist-frontend/src/components/BlogForm.js
+++ Part 5/bloglist-frontend/src/components/BlogForm.js
@@ -1,20 +1,45 @@
 import React from 'react'
 import Blog from './Blog'
 
-const blogForm = (handleLogout,user,blogs) => (
-    <form onSubmit={handleLogout}>
-          <p>
-            <span className="active-user">{user.name}</span> logged in
-            
-            <button type="submit">logout</button>
-          </p>
+
+const blogForm = (addBlog,blogs,author,handleAuthor, title, handleTitle,url,handleUrl,message) => {    
+    return (<>  
+    <form onSubmit={addBlog}>
+          
+          <h2>create new</h2>
+          <div>
+    Author
+    <input
+      type="author"
+      value={author}
+      name="author"
+      onChange={handleAuthor} />
+  </div>
+  <div>
+    Title
+    <input
+      type="title"
+      value={title}
+      name="title"
+      onChange={handleTitle} />
+  </div>
+  <div>
+    Url
+    <input
+      type="url"
+      value={url}
+      name="url"
+      onChange={ handleUrl } />
+  </div>
+  <button type="create">create</button>
+          
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
             )}
-    
-    </form>  
-  )
-
+  </form>
+  
+  </>  )
+}
 
 
 export default blogForm
