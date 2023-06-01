20c440f2778ea309544f54b15d1b65d972325cab
C.Ziskas <z1skgr@yahoo.gr>
2023-06-01 20:11:19 +0300

    Exer 5.18
    5.18 Make tests for logging in. Test both successful and unsuccessful login attempts. Make a new user in the beforeEach block for the tests. Check that the notification shown with unsuccessful login is displayed red.

--- Part 5/bloglist-frontend/cypress.config.js
+++ Part 5/bloglist-frontend/cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-undef
+const { defineConfig } = require('cypress')
 
+// eslint-disable-next-line no-undef
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
--- Part 5/bloglist-frontend/cypress/e2e/blog_api.cy.js
+++ Part 5/bloglist-frontend/cypress/e2e/blog_api.cy.js
@@ -1,13 +1,46 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Maria Man',
+      username: 'Maria',
+      password: 'Maria'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
 
   })
-
-  it('5.17 - Login from is shown', function() {
-    cy.contains('username')
-    cy.contains('password')
-    cy.contains('login').click()
+  describe('5.17', function(){
+    it('Login from is shown', function() {
+      cy.contains('username')
+      cy.contains('password')
+      cy.contains('login').click()
+    })
   })
+
+
+  describe('5.18',function() {
+    describe('Login', function(){
+      it('fails with wrong credentials', function() {
+        cy.contains('log in').click()
+        cy.get('#username').type('Mar')
+        cy.get('#password').type('Mar')
+        cy.get('#login-button').click()
+        cy.contains('ERROR, Wrong username or password')
+
+        cy.get('.error-message').should('contain', 'ERROR').and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      })
+
+      it('succeeds with correct credentials', function() {
+        cy.contains('log in').click()
+        cy.get('#username').type('Maria')
+        cy.get('#password').type('Maria')
+        cy.get('#login-button').click()
+        cy.contains('Login approved')
+      })
+    })
+
+  })
+
 })
