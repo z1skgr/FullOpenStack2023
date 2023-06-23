c00cff4d918b468b35ee3f9f61249e7d60367e9b
C.Ziskas <z1skgr@yahoo.gr>
2023-06-23 20:09:41 +0300

    Exer 8.6
    8.6 Implement mutation addBook

--- Part 8/graphql/index.js
+++ Part 8/graphql/index.js
@@ -1,5 +1,8 @@
-const { ApolloServer } = require('@apollo/server')
+const { ApolloServer} = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
+const { GraphQLError } = require('graphql')
+
 
 let authors = [
   {
@@ -118,9 +121,18 @@ const typeDefs = `
   type Author{
     name: String!
     id: ID!
-    bookCount: Int!
+    born: Int 
+    bookCount: Int
   }   
-`
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+  }
+`;
 
 const resolvers = {
   Query: {       
@@ -133,16 +145,36 @@ const resolvers = {
         ? books.filter((book) => book.author === args.author && book.genres.includes(args.genre))
         : (args.author) 
           ? books.filter((book) => book.author === args.author)
-          : books.filter((book) => book.genres.includes(args.genre))
-    }
+          : (args.genre) 
+            ? books.filter((book) => book.genres.includes(args.genre))
+            : books
+    }   
     
       
   },
   Author: {
-    bookCount : (root) =>{
-        books.filter(book => book.author === root.name).length
+    bookCount: (root) =>
+    books.filter((book) => book.author === root.name).length,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (books.find(b => b.title === args.title)) {
+        throw new GraphQLError('Book already exists', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.title
+          }
+        })
+      }
+
+      if (!authors.find((author) => author.name === args.author)) {
+        authors = authors.concat({ name: args.author, id: uuid() })
+      }
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
     }
-  } 
+  }
 
   
 }
